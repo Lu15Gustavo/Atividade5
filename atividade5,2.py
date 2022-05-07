5b5eaf8a10446897578fc48960b1c08f5c41e88c
Lu15Gustavo <102445868+Lu15Gustavo@users.noreply.github.com>
2022-05-07 00:38:56 -0300

    Add files via upload

---
+++ atividade5,2.py
@@ -0,0 +1,13 @@
+notas1 = input ("Insira à nota do primeiro bimestre: ")
+notas2 = input ("Insira à nota do segundo bimestre: ")
+notas3 = input ("Insira à nota do terceiro bimestre: ")
+notas4 = input ("Insira à nota do quarto bimestre: ")
+
+a = float(notas1)
+b = float(notas2)
+c = float(notas3)
+d = float(notas4)
+
+media = (a + b + c + d) /4
+
+print (media) 
